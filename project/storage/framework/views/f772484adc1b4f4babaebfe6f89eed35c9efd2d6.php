ae5bd3ab192f913a2ff2be145116557550396017
Moahmmed_Atef_Hassan <mahbfci1361998@gmail.com>
2020-04-17 17:37:31 +0200

    add

---
+++ project/storage/framework/views/f772484adc1b4f4babaebfe6f89eed35c9efd2d6.php
@@ -0,0 +1,59 @@
+<?php $__env->startSection('content'); ?>
+    <!-- page title area start -->
+    <div class="page-title-area">
+        <div class="row align-items-center">
+            <div class="col-sm-6">
+                <div class="breadcrumbs-area clearfix">
+                    <h4 class="page-title pull-left">Dashboard</h4>
+                    <ul class="breadcrumbs pull-left">
+                        <li><a href="index.html">Home</a></li>
+                        <li><span>Add Category</span></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 clearfix">
+                <div class="user-profile pull-right">
+                    <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
+                    <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Mahmoud <i class="fa fa-angle-down"></i></h4>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Message</a>
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item" href="#">Log Out</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- page title area end -->
+    <!-- page container area end -->
+    <!--start main content inner -->
+    <div class="col-12">
+        <div>
+            <h4 class="view-product-title pl--10 pt--20"># Adding New Category
+            </h4>
+        </div>
+        <div class="container mt-4 ml-5">
+            <form>
+                <div class="form-group">
+                    <label class="lbl-form">Category Name</label>
+                    <input type="text" class="form-control inp" placeholder="Enter Category Name" name="name" id="name" required>
+                </div>
+                <div class="form-group">
+                    <label class="lbl-form">Code</label>
+                    <input type="text" class="form-control inp" placeholder="Enter Category Code" name="code" id="code" required>
+                </div>
+
+                <div class="form-group">
+                    <label class="lbl-form">Product Quantity</label>
+                    <textarea name="quantity" id="quantity" class="form-control inp"></textarea>
+                </div>
+
+                <div class="reset-button">
+                    <input type="submit" class="btn btn-success mb-3" value="Add Category">
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--end main content inner -->
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.adminLayout.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\FCI\E-Commerce\Electronic-E-commerce-project\project\resources\views/admin/categories/add-category.blade.php ENDPATH**/ ?>
