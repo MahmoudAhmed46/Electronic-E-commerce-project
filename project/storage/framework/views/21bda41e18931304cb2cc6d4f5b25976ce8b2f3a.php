804b773ff6fe04cc3df5fbfce9a9de1274d399c4
Moahmmed_Atef_Hassan <mahbfci1361998@gmail.com>
2020-04-17 16:47:48 +0200

    add

---
+++ project/storage/framework/views/21bda41e18931304cb2cc6d4f5b25976ce8b2f3a.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="urf-8">
+    <title>History</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/user/style.css')); ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div id="container">
+    <div id="header">
+        <div id="subheader">
+            <!--<div class="upper-header">-->
+            <p>World fastest online shopping place</p>
+
+            <!-- </div>-->
+        </div>
+        <div id="main-header">
+            <!--Logo-->
+            <div id="logo">
+                <span id="fPart"><a href="home.html">Electronic</a></span><span id="sPart"><a href="home.html">Shop</a></span>
+            </div>
+            <!--Search-->
+            <div id="search">
+                <form action="">
+                    <input id="search-input" type="text" name="text" placeholder="What are you looking for ?">
+                    <input id="search-submit" type="submit" name="submit" value="Search">
+                </form>
+            </div>
+            <!--User Menu-->
+            <div id="all-cart">
+                <span id="cart-text">Cart</span>
+                <a href="<?php echo e(url('/cart')); ?>"><img src="<?php echo e(asset('assets/images/cart-logo.png')); ?>"></a>
+                <span id="num">0</span>
+            </div>
+            <!--Navigation-->
+            <div id="navigation">
+                <nav id="mainav">
+                    <a href="<?php echo e(asset('/user')); ?>">Home</a>
+                    <a href="#">New arrivals</a>
+                    <a href="#offers">Deals</a>
+                    <a href="#category">Electronics</a>
+                    <a href="#category">Accessroies</a>
+                    <a href="#products">Products</a>
+                    <a href="<?php echo e(url('/history')); ?>">History</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!--Product-->
+    <div id="prod-in-cart">
+        <!--Products-->
+        <div id="my-products">
+            <div id="heading-block">
+                <h2>My Cart</h2>
+            </div>
+            <div class="prod-container">
+
+                <div class="prod-box">
+                    <img src="<?php echo e(asset('assets/images/lenovo-ideapad.jpg')); ?>" alt="lenovo ideapad">
+                    <div class="prod-trans">
+                        <div class="prod-feature">
+                            <p>lenovo ideapad</p>
+                            <p style="color:#fff;font-weight: bold;
+                                        ">Price : 800$</p>
+                            <p style="color:#fff;font-weight: bold;
+                                    ">Items : 1</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="prod-box">
+                    <img src="<?php echo e(asset('assets/images/smart-washing.jpg')); ?>" alt="samsung smart washing machine">
+                    <div class="prod-trans">
+                        <div class="prod-feature">
+                            <p>samsung smart washing machine</p>
+                            <p style="color:#fff;font-weight: bold;
+                                        ">Price : 850$</p>
+                            <p style="color:#fff;font-weight: bold;
+                                    ">Items : 1</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="prod-box">
+                    <img src="<?php echo e(asset('assets/images/toshiba-vacuum-cleaner.jpg')); ?>" alt="toshiba vacuum cleaner">
+                    <div class="prod-trans">
+                        <div class="prod-feature">
+                            <p>toshiba vacuum cleaner</p>
+                            <p style="color:#fff;font-weight: bold;
+                                        ">Price : 100$</p>
+                            <p style="color:#fff;font-weight: bold;
+                                    ">Items : 1</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="prod-box">
+                    <img src="<?php echo e(asset('assets/images/samsung-lcd-screen.jpg')); ?>" alt="samsung lcd screen">
+                    <div class="prod-trans">
+                        <div class="prod-feature">
+                            <p>Samsung LCD Screen</p>
+                            <p style="color:#fff;font-weight: bold;
+                                        ">Price : 300$</p>
+                            <p style="color:#fff;font-weight: bold;
+                                    ">Items : 1</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="prod-box">
+                    <img src="<?php echo e(asset('assets/images/Huawei-earphones.jpg')); ?>" alt="Huawei-earphones">
+                    <div class="prod-trans">
+                        <div class="prod-feature">
+                            <p>Huawei Earphones</p>
+                            <p style="color:#fff;font-weight: bold;
+                                        ">Price : 20$</p>
+                            <p style="color:#fff;font-weight: bold;
+                                    ">Items : 1</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="prod-box">
+                    <img src="<?php echo e(asset('assets/images/Huawei-Nova-7i_1.jpg')); ?>" alt="Huawei Nova 7i">
+                    <div class="prod-trans">
+                        <div class="prod-feature">
+                            <p>Huawei Nova 7i</p>
+                            <p style="color:#fff;font-weight: bold;
+                                        ">Price : 700$</p>
+                            <p style="color:#fff;font-weight: bold;
+                                    ">Items : 1</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <div id="about">
+            <h2 style="color: darkred;">About</h2>
+            <p>Section 5</p>
+            <span>Mahmoud Mohamed Refaie</span>
+            <span>Mahmoud Sayed Fathy</span>
+            <span>Mahmoud Ahmed Mahmoud</span>
+            <span>Mohamed Atef Hassan</span>
+            <span>Omar Mohamed Tawfik</span>
+        </div>
+        <div id="contact">
+            <h2 style="color: darkred;">Contact Us</h2>
+            <ul>
+                <li><a id="facebook" href="https://www.facebook.com/LionMaax"><i class="fa fa-facebook-square" style="font-size: 30px;"></i></a></li>
+                <li><a id="github" href="https://github.com/MahmoudMRefaie/E-Commerce-Project"><i class="fa fa-github" style="font-size: 30px;"></i></a>
+                </li>
+                <li><a id="linkedin" href="https://www.linkedin.com/in/mahmoud-refaie-ba6618186/"><i class="fa fa-linkedin-square" style="font-size: 30px;"></i></a>
+                </li>
+            </ul>
+        </div>
+        <hr>
+        <p>&copy 2020 Acadmya. All rights reserved.</p>
+    </div>
+
+</div>
+
+</body>
+
+</html>
+<?php /**PATH F:\FCI\E-Commerce\Electronic-E-commerce-project\project\resources\views/history.blade.php ENDPATH**/ ?>
---
+++ project/storage/framework/views/2c39d535aa3197b94e766a73997aa7ad9c7dd9b5.php
@@ -0,0 +1,74 @@
+<?php $__env->startSection('content'); ?>
+    <!-- page title area start -->
+    <div class="page-title-area">
+        <div class="row align-items-center">
+            <div class="col-sm-6">
+                <div class="breadcrumbs-area clearfix">
+                    <h4 class="page-title pull-left">Dashboard</h4>
+                    <ul class="breadcrumbs pull-left">
+                        <li><a href="index.html">Home</a></li>
+                        <li><span>Add Product</span></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 clearfix">
+                <div class="user-profile pull-right">
+                    <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
+                    <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Mahmoud <i class="fa fa-angle-down"></i></h4>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Message</a>
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item" href="#">Log Out</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- page title area end -->
+    <!-- page container area end -->
+    <!--start main content inner -->
+    <div class="col-12">
+        <div>
+            <h4 class="view-product-title pl--10 pt--20"># Adding New Product</h4>
+        </div>
+        <div class="container mt-4 ml-5">
+            <form>
+                <div class="form-group">
+                    <label class="lbl-form">Product Name</label>
+                    <input type="text" class="form-control inp" placeholder="Enter Product Name" name="product_name" id="product_name" required>
+                </div>
+                <div class="form-group">
+                    <label class="lbl-form">Under Category</label>
+                    <select name="cat_id" id="cat_id" class="form-control inp">
+                        <option value='' selected disabled> select </option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label class="lbl-form">Code</label>
+                    <input type="text" class="form-control inp" placeholder="Enter Product Code" name="product_code" id="product_code" required>
+                </div>
+                <div class="form-group">
+                    <label class="lbl-form">Product Price</label>
+                    <input type="number" class="form-control inp" placeholder="Enter Product Price" name="product_price" id="product_price" required>
+                </div>
+                <div class="form-group">
+                    <label class="lbl-form">Product Quantity</label>
+                    <input type="number" class="form-control inp" placeholder="Enter Product Quantity" name="quantity" id="quantity" required>
+                </div>
+                <div class="form-group">
+                    <label class="lbl-form">Product Description</label>
+                    <textarea name="product_description" id="product_description" class="form-control inp"></textarea>
+                </div>
+                <div class="form-group">
+                    <label class="lbl-form">Picture upload</label>
+                    <input type="file" name="img">
+                </div>
+                <div class="reset-button">
+                    <input type="submit" class="btn btn-success mb-3" value="Add Product">
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--end main content inner -->
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.adminLayout.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\FCI\E-Commerce\Electronic-E-commerce-project\project\resources\views/admin/products/add-products.blade.php ENDPATH**/ ?>
